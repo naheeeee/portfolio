d964092fb2b9d7520ed90383f6972d2c5204fecf
naheeeee <fpqghd@naver.com>
2022-12-16 08:58:46 +0900

    221216_수정

--- common.js
+++ common.js
@@ -1,17 +1,72 @@
-// gnb .on 효과
-const $sec = document.querySelectorAll(".sec");
+const elm = document.querySelectorAll(".output .sec");
+const elmCount = elm.length;
 
-$sec.forEach(function (item, idx) {
-  let $gnbA = document.querySelectorAll(".gnb li");
-  if (scrollTop >= item.offsetTop) {
-    $gnbA.forEach(function (a) {
-      a.classList.remove("on");
-    });
-    document
-      .querySelector(`.gnb li:nth-of-type(${idx + 2})`)
-      .classList.add("on");
-  }
+elm.forEach((item, idx) => {
+  item.addEventListener("mousewheel", function (e) {
+    e.preventDefault();
+    let delta = 0;
+    if (e.wheelDelta) {
+      delta = e.wheelDelta;
+    }
+    let moveTop = window.scrollY;
+    let elmSelector = elm[idx];
+
+    // 휠을 내렸을 때
+    if (delta < 0) {
+      if (elmSelector !== elmCount - 1) {
+        try {
+          moveTop =
+            window.pageYOffset +
+            elmSelector.nextElementSibling.getBoundingClientRect().top;
+        } catch (e) {}
+      }
+    } else {
+      // 휠을 올렸을 때
+      if (elmSelector !== 0) {
+        try {
+          moveTop =
+            window.pageYOffset +
+            elmSelector.previousElementSibling.getBoundingClientRect().top;
+        } catch (e) {}
+      }
+    }
+    window.scrollTo({ top: moveTop });
+  });
 });
 
-// sec2 스크롤 내리면서 about, work, contact 글자 사라지고
-// 큰 비눗방울 한 군데로 모아서 내리기
+window.addEventListener("scroll", function (e) {
+  let scrollTop =
+    document.documentElement.scrollTop ||
+    this.window.scrollY ||
+    this.window.pageYOffset;
+
+  elm.forEach((item2, i) => {
+    if (scrollTop >= item2.offsetTop - 50) {
+      document.querySelectorAll(".gnb li").forEach((li) => {
+        li.classList.remove("on");
+      });
+      document
+        .querySelector(".gnb li:nth-child(" + (i + 1) + ")")
+        .classList.add("on");
+    }
+  });
+});
+
+// sec4 탭
+const $sec4Bigmain = document.querySelectorAll(".sec4_bigmain"),
+  $sec4Button = document.querySelectorAll(".sec4_right>div>button");
+
+$sec4Button.forEach(function (item, idx) {
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+    $sec4Button.forEach(function (test) {
+      test.classList.remove("on");
+    });
+    this.classList.add("on");
+
+    $sec4Bigmain.forEach(function (test) {
+      test.classList.remove("on");
+    });
+    $sec4Bigmain[idx].classList.add("on");
+  });
+});
