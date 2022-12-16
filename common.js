6d87d1789d164772e8351e0c5c56568b8b8e0338
naheeeee <fpqghd@naver.com>
2022-12-16 14:12:32 +0900

    221216_수정

--- common.js
+++ common.js
@@ -1,5 +1,6 @@
 const elm = document.querySelectorAll(".output .sec");
 const elmCount = elm.length;
+const mediaViewContent = window.matchMedia(`(max-width: 414px)`);
 
 elm.forEach((item, idx) => {
   item.addEventListener("mousewheel", function (e) {
@@ -36,12 +37,107 @@ elm.forEach((item, idx) => {
 
 window.addEventListener("scroll", function (e) {
   let scrollTop =
-    document.documentElement.scrollTop ||
-    this.window.scrollY ||
-    this.window.pageYOffset;
+    document.documentElement.scrollTop || window.scrollY || window.pageYOffset;
+
+  let ht = document.querySelector(".output .sec").clientHeight;
+  let $moveBubble = document.querySelector(".moveBubble");
+  let $bubble1 = document.querySelector(".bubble1");
+  let $bubble1Img = document.querySelector(".bubble1>img");
+  let $bubble2 = document.querySelector(".bubble2");
+  let $bubble3 = document.querySelector(".bubble3");
+  let $bubble3sub = document.querySelector(".shape");
+
+  if (scrollTop >= 0 && scrollTop < ht * 1) {
+    // $moveBubble.style.cssText = `top:calc(50vh - 5vw); left:calc(50vw - 5vw); transition: 0.5s;`;
+    $bubble1.style.cssText = `  top: 9vw;
+    left: 40vw; transition: 0.5s;`;
+    $bubble2.style.cssText = `top: 23.5vw;
+    left: 49vw; transition: 0.5s;`;
+    $bubble3.style.cssText = ` top: 8vw;
+    left: 68vw; transition: 0.5s;`;
+    $bubble3sub.style.cssText = ` opacity: 0`;
+  }
+  if (scrollTop >= ht * 1 && scrollTop < ht * 2) {
+    // $moveBubble.style.cssText = `top:calc(50vh - 5vw); left:calc(50vw - 5vw); transition: 0.5s;`;
+    $bubble1.style.cssText = `  width: 10vh;
+    height: 10vh; top:65vh; left:25vw; transition: 0.5s; animation: rad-move 10s linear infinite `;
+    // $bubble1Img.style.cssText = `animation: rad-move 10s linear infinite `;
+    $bubble2.style.cssText = `  width: 10vh;
+    height: 10vh; top:65vh; left:25vw; transition: 0.5s; opacity:0 `;
+    $bubble3.style.cssText = `  width: 10vh;
+    height: 10vh; top:65vh; left:25vw; transition: 0.5s; opacity:0 `;
+    $bubble3sub.style.cssText = ` opacity: 0`;
+  }
+  if (scrollTop >= ht * 2 && scrollTop < ht * 3) {
+    // $moveBubble.style.cssText = `top:calc(50vh - 5vw); left:calc(50vw - 5vw); transition: 0.5s;`;
+    $bubble1.style.cssText = `  width: 17vh;
+    height: 17vh; top:10vh; left:5.5vw; transition: 0.5s; opacity:0 `;
+    $bubble2.style.cssText = `  width: 17vh;
+    height: 17vh; top:10vh; left:5.5vw; transition: 0.5s; opacity:1 `;
+    $bubble3.style.cssText = `  width: 17vh;
+    height: 17vh; top:10vh; left:5.5vw; transition: 0.5s; opacity:0 `;
+    $bubble3sub.style.cssText = ` opacity: 0`;
+  }
+  if (scrollTop >= ht * 3 && scrollTop < ht * 4) {
+    // $moveBubble.style.cssText = `top:calc(50vh - 5vw); left:calc(50vw - 5vw); transition: 0.5s;`;
+    $bubble1.style.cssText = `  width: 20vw;
+    height: 20vw; top:10vh; left:5.5vw; transition: 0.5s; opacity:0 `;
+    $bubble2.style.cssText = `  width: 8vw;
+    height: 8vw; top:10vh; left:5.5vw; transition: 0.5s; opacity:0 `;
+    $bubble3.style.cssText = `  width: 50vh;
+    height: 50vh; top:25vh; left:50vw; transition: 0.5s; opacity:1 `;
+    $bubble3sub.style.cssText = ` top:0vh; left:0vw; transition: 0.5s; opacity:0.6`;
+  }
+
+  const viewChangeHandler = (mediaViewContent) => {
+    if (scrollTop >= 0 && scrollTop < ht * 1) {
+      // $moveBubble.style.cssText = `top:calc(50vh - 5vw); left:calc(50vw - 5vw); transition: 0.5s;`;
+      $bubble1.style.cssText = `  width: 20vw;
+      height: 20vw; top: 30vh;
+          left: 10vw; transition: 0.5s;`;
+      $bubble2.style.cssText = `top: 23.5vh;
+          left: 49vw; transition: 0.5s;`;
+      $bubble3.style.cssText = ` top: 30vh;
+          left: 80vw; transition: 0.5s;`;
+      $bubble3sub.style.cssText = ` opacity: 0`;
+    }
+    if (scrollTop >= ht * 1 && scrollTop < ht * 2) {
+      // $moveBubble.style.cssText = `top:calc(50vh - 5vw); left:calc(50vw - 5vw); transition: 0.5s;`;
+      $bubble1.style.cssText = `  width: 10vh;
+          height: 10vh; top:65vh; left:25vw; transition: 0.5s; animation: rad-move 10s linear infinite `;
+      // $bubble1Img.style.cssText = `animation: rad-move 10s linear infinite `;
+      $bubble2.style.cssText = `  width: 10vh;
+          height: 10vh; top:65vh; left:25vw; transition: 0.5s; opacity:0 `;
+      $bubble3.style.cssText = `  width: 10vh;
+          height: 10vh; top:65vh; left:25vw; transition: 0.5s; opacity:0 `;
+      $bubble3sub.style.cssText = ` opacity: 0`;
+    }
+    if (scrollTop >= ht * 2 && scrollTop < ht * 3) {
+      // $moveBubble.style.cssText = `top:calc(50vh - 5vw); left:calc(50vw - 5vw); transition: 0.5s;`;
+      $bubble1.style.cssText = `  width: 17vh;
+          height: 17vh; top:10vh; left:5.5vw; transition: 0.5s; opacity:0 `;
+      $bubble2.style.cssText = `  width: 17vh;
+          height: 17vh; top:10vh; left:5.5vw; transition: 0.5s; opacity:1 `;
+      $bubble3.style.cssText = `  width: 17vh;
+          height: 17vh; top:10vh; left:5.5vw; transition: 0.5s; opacity:0 `;
+      $bubble3sub.style.cssText = ` opacity: 0`;
+    }
+    if (scrollTop >= ht * 3 && scrollTop < ht * 4) {
+      // $moveBubble.style.cssText = `top:calc(50vh - 5vw); left:calc(50vw - 5vw); transition: 0.5s;`;
+      $bubble1.style.cssText = `  width: 20vw;
+          height: 20vw; top:10vh; left:5.5vw; transition: 0.5s; opacity:0 `;
+      $bubble2.style.cssText = `  width: 8vw;
+          height: 8vw; top:10vh; left:5.5vw; transition: 0.5s; opacity:0 `;
+      $bubble3.style.cssText = `  width: 50vh;
+          height: 50vh; top:25vh; left:50vw; transition: 0.5s; opacity:1 `;
+      $bubble3sub.style.cssText = ` top:0vh; left:0vw; transition: 0.5s; opacity:0.6`;
+    }
+  };
+
+  mediaViewContent.addEventListener("change", viewChangeHandler);
 
   elm.forEach((item2, i) => {
-    if (scrollTop >= item2.offsetTop - 50) {
+    if (scrollTop >= item2.offsetTop - 5) {
       document.querySelectorAll(".gnb li").forEach((li) => {
         li.classList.remove("on");
       });
